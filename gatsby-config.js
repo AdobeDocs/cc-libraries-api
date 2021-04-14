583b16728767fe3ce2291a40ad6617a170d07d08
Stephan Ringel <ringel@adobe.com>
2021-04-14 13:59:47 +0200

    Update deploy workflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -160,5 +160,5 @@ module.exports = {
     ],
   },
   plugins: [`@adobe/gatsby-theme-aio`],
-  pathPrefix: process.env.PATH_PREFIX || "/creative-cloud-libraries/docs",
+  pathPrefix: process.env.PATH_PREFIX || "/creative-cloud-libraries/docs/",
 };
